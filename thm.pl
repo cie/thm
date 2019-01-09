d66bcb9797fe4d97679eb182f82c1280c8b8e67b
Bernát Kalló <kallo.bernat@gmail.com>
2019-01-10 00:36:42 +0100

    operators

--- thm.pl
+++ thm.pl
@@ -1,22 +1,31 @@
 :- initialization(main).
+:- op(1000, xfy, and).
+:- op(1100, xfy, or).
+:- op(700, xfy, in).
 
 def(x, true).
 def(y, false).
 axiom(true).
 axiom(not(false)).
+axiom((1 in integers)).
 
-proven(X is Y) :- def(X, Y).
-proven(X is Y) :- def(Y, X).
+proven(X = Y) :- def(X, Y).
+proven(X = Y) :- def(Y, X).
 
 proven(X) :- axiom(X).
 proven(X) :- def(X, D), proven(D).
-proven(not(X)) :- proven(X is false).
+proven((X and Y)) :- proven(X), proven(Y).
+proven((X or Y)) :- proven(X); proven(Y).
 
 test(proven(true), true).
 test(proven(x), true).
 test(proven(false), fail).
 test(proven(y), fail).
-test(proven(not(y)), true).
+test((Y = false, proven(not(Y))), true).
+test(proven((x or y)), true).
+test(proven((x and y)), fail).
+test(proven((x and x)), true).
+test(proven((1 in integers)), true).
 
 if(A, Then, Else) :- A, !, Then; Else.
 not(A):- if(A, fail, true).
